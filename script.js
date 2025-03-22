9978921eea85206c831cb947154dba737c530916
RichyQ8 <richyg824@gmail.com>
2025-03-22 22:16:07 +0530

    maybe corrected sendmail

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function sendMail(){
     let parms={
-        subject : document.getElementById("subject").value,
+        
         name : document.getElementById("name").value,
     }
 
-    emailjs.send("service_pb250zp","template_s4j9xkd",parms).then(alert("Email Sent"))
+    emailjs.send("service_pb250zp","template_nna63fa",parms).then(alert("Email Sent"))
 }
