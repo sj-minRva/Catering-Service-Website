ea403b2799b64a21418f3579d2cf795baaa420e3
sj-minRva <rheamaria13@gmail.com>
2025-03-25 17:46:26 +0530

    admin login fix

--- test api/app.py
+++ test api/app.py
@@ -1,10 +1,37 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from database import insert_customer
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+# Function to insert customer data into the database
+def insert_customer(name, email, password):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='Catering',
+            user='root',
+            password='root'
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            insert_query = """INSERT INTO customers (name, email, password) VALUES (%s, %s, %s)"""
+            record = (name, email, password)
+            cursor.execute(insert_query, record)
+            connection.commit()
+            print("Customer data inserted successfully")
+        
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+    
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+
+# API to handle customer registration
 @app.route('/api/customers', methods=['POST'])
 def create_customer():
     customer_data = request.json
